03123ee4d65d81a2ec2974b7ce0b03611c466021
CostaArmenUniMelb <carmen@student.unimelb.edu.au>
2017-03-26 14:00:38 +1100

    unfinished main procedure

--- snack_pprint_test.ml
+++ snack_pprint_test.ml
@@ -15,8 +15,6 @@ let procedure_q_body = (procedure_q_declerations,procedure_q_statements)
 
 let procedure_q = (procedure_q_identifier,procedure_q_parameters,procedure_q_body)
   
-(*let test_procedure_q = print_string (format_procedure procedure_q)*)
-  
 (*procedure p*)
 let procedure_p_identifier = "p" 
 let procedure_p_parameters = [(Reference,Single(Int,"i"))]
@@ -30,12 +28,27 @@ let procedure_p_body = (procedure_p_declerations,procedure_p_statements)
 
 let procedure_p = (procedure_p_identifier,procedure_p_parameters,procedure_p_body)
   
-(*let test_procedure_p = print_string (format_procedure procedure_p)*)
+(*procedure main*)
+let procedure_main_identifier = "main" 
+let procedure_main_parameters = []
   
+let procedure_main_declerations = [Single(Int,"m"); Single(Int,"n")]
+  
+let read_n = Read(LId("n"))
+let n_gt_1 = Ebinop(Elval(LId("n")),Op_gt,Eint(1))
+let whiledo_1 = WhileDo(n_gt_1,[])
+  
+let procedure_main_statements = [read_n; whiledo_1]
+  
+let procedure_main_body = (procedure_main_declerations,procedure_main_statements)
 
-let test_program = [procedure_q; procedure_p]
-    
-let _ = print_string (format_program test_program)
+let procedure_main = (procedure_main_identifier, procedure_main_parameters, procedure_main_body)
+  
+(*all procedures*)
+let program = [procedure_q; procedure_p; procedure_main]
+  
+(*print test*)
+let _ = print_string (format_program program)
   
   
   
