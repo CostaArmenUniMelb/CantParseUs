0e63843cbc64ecaeeb3f6da98c94a921889fc673
Poom <poomnewgen@gmail.com>
2017-05-18 21:50:51 +1000

    Add output fiel

--- snick.ml
+++ snick.ml
@@ -1,12 +1,14 @@
 module P = Snick_parse
 open Snick_symbol
 open Codegen
+open Printf
 
 (* Argument parsing code *)
 let infile_name = ref None
 
 type compiler_mode = PrettyPrint | Compile
 let mode = ref Compile
+let file_output = "output.brill"
 
 (* --------------------------------------------- *)
 (*  Specification for command-line options       *)
@@ -33,7 +35,14 @@ let main () =
     let (prog,main_sym_tbl) = Snick_parse.program_with_sym_tbl Snick_lex.token lexbuf in
     match !mode with
     | PrettyPrint -> print_string (Snick_pprint.print_program prog)
-    | Compile -> print_string (generate(prog))
+    | Compile -> 
+      let generated_ir = generate(prog) in
+      let oc = open_out file_output in 
+      (* print_string (generated_ir); *)
+      print_string (sprintf "Generated output to %s successfully!!\n" file_output);
+      fprintf oc "%s\n" generated_ir;
+      close_out oc;
+
   with
     | Parsing.Parse_error -> Snick_lex.raise_parser_fail lexbuf 
     | Snick_analyze.Syntax_error _ as err -> let msg = Printexc.to_string err in Snick_lex.raise_syntax_fail lexbuf msg
